6df8d839f39380ec71cf9235cda46a42a8926cad
kncloudproject <kncloudproject@gmail.com>
2025-06-19 12:42:06 -0400

    Updated Backend

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,9 +1,9 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = "kn-pbl-terraform-statefile-ci"
+    bucket         = "kn-pbl-terraform-statefile-jupiter"
     key            = "jupiter/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "kn-pbl-terraform-locks"
+    dynamodb_table = "kn-pbl-terraform-locks-jupiter"
   }
 }
